1364e80b520404eca10867bb39816350b4137650
Quang HUYNH <htquangg@gmail.com>
2022-08-03 22:53:10 +0700

    fix: config new security routes

--- src/utils/configRoutes.ts
+++ src/utils/configRoutes.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response, NextFunction } from 'express';
-import { AuthService } from '~/services/Auth';
+import { verifyAuthPrivate, verifyAuthToken } from '~/services/Auth/service';
 
 export type Handler = (
   req: Request,
@@ -7,7 +7,11 @@ export type Handler = (
   next: NextFunction,
 ) => Promise<void> | void;
 
-export type SecurityPermission = 'UNPROTECTED' | 'PRIVATE' | 'MIDDLEWARE';
+export type SecurityPermission =
+  | 'UNPROTECTED'
+  | 'PROTECTED'
+  | 'PRIVATE'
+  | 'MIDDLEWARE';
 
 export type Route = {
   path: string;
@@ -21,7 +25,6 @@ export const applyRouteSets = (routeSets: Route[][], express: Express) => {
     applyRoutes(routeSet, express);
   }
 };
-
 export const applyRoutes = (routes: Route[], express: Express) => {
   for (const route of routes) {
     const { method, path, handler, security } = route;
@@ -29,8 +32,15 @@ export const applyRoutes = (routes: Route[], express: Express) => {
       case 'UNPROTECTED':
         (express as any)[method](path, handler);
         break;
+      case 'PROTECTED':
+        if (verifyAuthToken) {
+          (express as any)[method](path, verifyAuthToken, handler);
+        }
+        break;
       case 'PRIVATE':
-        (express as any)[method](path, AuthService.verifyAuthToken, handler);
+        if (verifyAuthPrivate) {
+          (express as any)[method](path, verifyAuthPrivate, handler);
+        }
         break;
       case 'MIDDLEWARE':
         (express as any).use(path, handler);
